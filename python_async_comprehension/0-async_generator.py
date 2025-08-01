e56b0ed8bc42b901e3f9b8eb0371d192f5d72357
Mohammed0K <M.H.D-6@hotmail.com>
2025-08-01 18:27:53 +0300

    update

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Coroutine async_generator that yields random numbers asynchronously."""
+"""Coroutine that yields random numbers asynchronously."""
 
 import asyncio
 import random
