5233ab427d01599614be090f0a8f5b8207886450
Mohammed0K <M.H.D-6@hotmail.com>
2025-08-01 15:53:15 +0300

    add document

--- python_variable_annotations/9-main.py
+++ python_variable_annotations/9-main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
+"""This module provides a function to return the length of elements in an iterable."""
 
-"""Print the annotations of a function."""
-element_length = __import__("9-element_length").element_length
+from typing import Iterable, Sequence, List, Tuple
 
-print(element_length.__annotations__)
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """Returns a list of tuples (item, length of item) for each sequence in lst."""
+    return [(i, len(i))
+            for i in lst]
